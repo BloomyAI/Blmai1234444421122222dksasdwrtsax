1e3810b67b33b14aa99a0d261a986e11a8a697d8
supportccoaching-create <supportccoaching@gmail.com>
2026-06-25 15:55:18 -0600

    Remove Edit and View menu from desktop app

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -38,31 +38,6 @@ function createMenu() {
         { type: 'separator' },
         { role: 'quit' }
       ]
-    },
-    {
-      label: 'Edit',
-      submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' }
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        { role: 'reload' },
-        { role: 'forceReload' },
-        { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
-      ]
     }
   ];
 
