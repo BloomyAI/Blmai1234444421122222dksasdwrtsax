e126daae460e3127f1c34e7bdedb963e427a957f
bloomy <supportccoaching@gmail.com>
2026-06-29 13:34:10 -0600

    Add favicon to show Bloomy AI logo in browser tab

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/logo.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </head>
       <body>
